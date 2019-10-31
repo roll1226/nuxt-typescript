3a3e4d2d7706883392ac7f282eb18f0c79da2964
roll1226 <tomo_yasu_4724@icloud.com>
2019-10-31 16:06:42 +0900

    change/firebase

--- pages/firebase.vue
+++ pages/firebase.vue
@@ -8,26 +8,38 @@
     <button @click="goFire">
       送信
     </button>
+    <button @click="getFire">
+      受信
+    </button>
     <br />
     {{ text }}
+    <div v-for="(texts, index) in gette" :key="index">
+      <p>
+        {{ texts.text }}
+      </p>
+      <p>
+        {{ texts.createdAt }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Emit } from 'nuxt-property-decorator'
+import { Component, Vue } from 'nuxt-property-decorator'
 import { firestore } from '@/plugins/firebase'
 
 @Component
 export default class firebase extends Vue {
   public text?: string = ''
+  public gette?: object[] = []
 
-  @Emit()
   goFire() {
     firestore
       .collection('test')
-      .doc('test')
+      .doc(`test2`)
       .set({
-        text: this.text
+        text: this.text,
+        createdAt: new Date()
       })
       .then(function() {
         console.log('Document successfully written!')
@@ -35,6 +47,21 @@ export default class firebase extends Vue {
       .catch(function(error) {
         console.error('Error writing document: ', error)
       })
+  }
+
+  getFire() {
+    this.gette = []
+    firestore
+      .collection('test')
+      .get()
+      .then((docs) => {
+        docs.docs.map((doc) => {
+          if (this.gette) {
+            this.gette.push(doc.data())
+          }
+        })
+      })
+    console.log(this.gette)
   }
 }
 </script>
